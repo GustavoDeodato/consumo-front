ec9ff15e3a78311fafb516674679753787a956fd
GUSTAVO SILVA DEODATO <24122736@CFP127ALUNO.LOCAL>
2025-05-06 16:57:04 -0300

    é

---
+++ js/contato.js
@@ -0,0 +1,95 @@
+'use strict'
+
+export async function getContatos() {
+    const url = 'https://bakcend-fecaf-render.onrender.com/contatos/'
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+export async function getContatosPorNome(nome) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos?nome_like=^${nome}`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+export async function postContato(contato) {
+    const url = 'https://bakcend-fecaf-render.onrender.com/contatos'
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contato)
+    }
+    const response = await fetch(url, options)
+
+    return response.ok
+}
+
+async function putContato(contato, id) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contato)
+    }
+    const response = await fetch(url, options)
+
+    return response.ok
+}
+
+async function deleteContato(id) {
+    const url = `https://bakcend-fecaf-render.onrender.com/contatos/${id}`
+    const options = {
+        method: 'DELETE'
+    }
+    const response = await fetch(url, options)
+    return response.ok
+
+}
+
+const contato = {
+    "nome": "Jua",
+    "celular": "123 6969 9669",
+    "foto": "cara.png",
+    "email": "tangamandapio@gmail.com",
+    "endereco": "Av. Travecos, 234",
+    "cidade": "Berlim"
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
